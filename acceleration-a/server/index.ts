9c1ff590f61f682f2e587ad7c59aa0abbf66b0b4
Raisal PA <raisal.pa@cfcomputing.co.uk>
2019-05-22 12:35:38 +0530

    fix timeout

--- acceleration-a/server/index.ts
+++ acceleration-a/server/index.ts
@@ -43,7 +43,3 @@ server.listen(port, (err: Error) => {
 
   console.log(`server is listening on ${port}`);
 });
-
-setTimeout(() => {
-  status.fail = false;
-}, Math.random() * 300000);
--- acceleration-dv/server/index.ts
+++ acceleration-dv/server/index.ts
@@ -42,7 +42,3 @@ server.listen(port, (err: Error) => {
 
   console.log(`server is listening on ${port}`);
 });
-
-setTimeout(() => {
-  status.fail = false;
-}, Math.random() * 300000);
