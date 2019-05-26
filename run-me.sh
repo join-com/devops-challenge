0d3b426da476d6ef3b80ad2591fc5a0c2f4f83e9
Raisal PA <raisal.pa@cfcomputing.co.uk>
2019-05-26 22:36:16 +0530

    small tweaks

--- run-me.sh
+++ run-me.sh
@@ -17,9 +17,14 @@ runmyapp
 
 runmyapp(){
 sleep 2s;
+echo "[INFO]: Enabling Addon Ingress for Minikube";
+minikube addons enable ingress
 echo "[INFO]: Starting the Acceleration Application using Helm";
 helm install acceleration-helm
 sleep 5s;
 echo "[Success]: Acceleration App Deployed";
+printf "\n\n"
+echo "[Success]: Browse http://`minikube ip`/calc?vf=200&vi=5&t=123"
+printf "\n\n"
 }
 prepare
